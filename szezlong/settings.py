f784845d9d7fc3c4ef20374cad313f8c76dc2ee7
Adam Beret Manczuk <beret@hipisi.org.pl>
2021-04-08 21:40:52 +0100

    temporary turn off bootstrap5

--- szezlong/settings.py
+++ szezlong/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'crispy_forms',
-    "crispy_bootstrap5",
+    # "crispy_bootstrap5",
     "crispy_custom",
     'hospitals',
     'beds',
